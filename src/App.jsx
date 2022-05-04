68f58c76e5dd6e89902d9176d55249ec7e8b0a99
Varun <varunsahi1997@gmail.com>
2022-05-04 19:05:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/styles.css";
+
+export default function App(){
+    return(
+        <div className="main-box">
+            <div className="tracker-body">
+                Expense Tracker
+            </div>
+        </div>
+    );
+}
