5ab8f011acc450d85e732b0e43f439d5e2e350af
Varun <varunsahi1997@gmail.com>
2022-05-05 15:09:44 +0530

    added Expenses.jsx file

---
+++ src/components/expenses/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "../ui/Card";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2> {props.title} </h2>
+      </div>
+      <div className="expense-item__price"> ${props.amount} </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,12 @@
+const Card = (props) => {
+
+    const classes = "card " +props.className
+
+    return(
+        <div className={classes}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Card;
